456122490af2a63b95d17ef607c4c1d5e3671d93
Mateusz Kuplowski <dremat@Dremat.local>
2022-05-29 20:03:34 +0200

    adding user data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './styles/index.scss';
 import Header from './components/Header';
+import WrapperUser from './components/WrapperUser';
 function App() {
   return (
     <>
       <Header />
+      <WrapperUser />
+      
         
       
     </>
